1cf53c922603a960a99cc8f3da0c53d2516d6268
Rahul <imrahulg01@gmail.com>
2025-04-28 11:16:14 +0000

    fix: updated urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("linkedin/list-adAccounts/", LinkedInAdAccountsAPIView.as_view(), name="linkedin-login"),
+    path("linkedin/list-adAccounts/", LinkedInAdAccountsAPIView.as_view(), name="linkedin-adAccounts"),
+    path("linkedin/adAccounts-analytics", LinkedInAdAnalyticsView.as_view(), name="adAccounts-analytics")
 ]
